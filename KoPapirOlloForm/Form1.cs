2bec0d43982cfed17d1fb92237fdf2fe46f18d9b
guthember <guthember@gmail.com>
2020-10-06 10:24:44 +0200

    Add project files.

---
+++ KoPapirOlloForm/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KoPapirOlloForm
+{
+  public partial class frmFo : Form
+  {
+    public frmFo()
+    {
+      InitializeComponent();
+    }
+  }
+}
